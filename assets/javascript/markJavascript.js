0cd27a69b9d52e653be4536691c20e857257d059
Mark <contact.mshin@gmail.com>
2018-10-04 21:24:19 -0600

    basic framework

---
+++ assets/javascript/markJavascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyDw6GM_E3CMyhSf9ErZJJNdY3bEfJyIZvM",
+    authDomain: "project1-movies.firebaseapp.com",
+    databaseURL: "https://project1-movies.firebaseio.com",
+    projectId: "project1-movies",
+    storageBucket: "project1-movies.appspot.com",
+    messagingSenderId: "175120493064"
+  };
+  firebase.initializeApp(config);
+});
