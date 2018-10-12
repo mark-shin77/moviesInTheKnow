327b5ff99b353c1aeeac7456cecdb2b988421153
Mark <contact.mshin@gmail.com>
2018-10-11 19:32:10 -0600

    on click functions 2

--- assets/javascript/movie.js
+++ assets/javascript/movie.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
     method: "GET"
   }).then(function(response){  
     console.log(response);
-    for (var x = 0; x < 12; x++){
+    for (var x = 0; x < 4; x++){
       if(response.results[x].poster_path !== null){
         $('#item1').append("<img id='movie" + x + "' src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
         $('#item2').append(response.results[x].title);
@@ -104,42 +104,77 @@ $(document).ready(function(){
     // console.log(response);
     for (var x = 0; x < 5; x++){
       if(response.results[x].poster_path !== null){
-        $('.third-section1').append("<img src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
-        $('.third-section1').append(response.results[x].title);
-        $('img').addClass('movie-poster');
-      }
+        $('#item7').append("<img src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
+        $('#item8').append(response.results[x].title);
+        $('#movie'+ x).addClass('movie-poster');
+        $('#movie' + x).attr('data-id', x)      }
     }
     for (var x = 5; x < 9; x++){
       if(response.results[x].poster_path !== null){
-        $('.third-section2').append("<img src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
-        $('.third-section2').append(response.results[x].title);
+        $('#item9').append("<img src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
+        $('#item10').append(response.results[x].title);
+        $('#movie'+ x).addClass('movie-poster');
+        $('#movie' + x).attr('data-id', x)
       }
     }
     for (var x = 9; x < 13; x++){
       if(response.results[x].poster_path !== null){
-        $('.third-section3').append("<img src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
-        $('.third-section3').append(response.results[x].title);
+        $('#item11').append("<img src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
+        $('#item12').append(response.results[x].title);
+        $('#movie'+ x).addClass('movie-poster');
+        $('#movie' + x).attr('data-id', x)
       }
     }
   });
 
-    // Pagination for Coming Soon 
+  // on click function for movie posters
+  $('.movie-poster').on('click', function(){
+    console.log('working');
+    // Hides Front Page and displays movie info screen
+    $('.movie-info-screen').css('display', 'inline-block');
+    $('.carousel').css('display', 'none');
+    $('.first-page').css('display', 'none');
+
+    var movies = response.results
+    var clicked = $(this).attr('data-id');
+    // Display Poster
+    $('.poster').html("<img src='http://image.tmdb.org/t/p/w185/" + movies[clicked].poster_path + "'></img>");
+    // Display Title
+    $('.title').html(movies[clicked].title);
+    // Display Overview
+    $('.overview').html('<br>' + movies[clicked].overview);
+    // Display Overview
+    $('.release-date').html('<br>' + 'Release Date: ' + movies[clicked].release_date);
+    // Display Overview
+    $('.vote-average').html('<br>' + 'Voters Average: ' + movies[clicked].vote_average);
+  })
+
+    // Pagination for Now Playing 
     $('.part4').on('click', function(){
-      $('.third-section1').css('display', 'inline-block');
-      $('.third-section2').css('display', 'none');
-      $('.third-section3').css('display', 'none');
+      $('#item7').css('display', 'inline-block');
+      $('#item8').css('display', 'inline-block');
+      $('#item9').css('display', 'none');
+      $('#item10').css('display', 'none');
+      $('#item11').css('display', 'none');
+      $('#item12').css('display', 'none');
     })
     $('.part5').on('click', function(){
       // console.log('working');
-      $('.third-section1').css('display', 'none');
-      $('.third-section2').css('display', 'inline-block');
-      $('.third-section3').css('display', 'none');
+      $('#item7').css('display', 'none');
+      $('#item8').css('display', 'none');
+      $('#item9').css('display', 'inline-block');
+      $('#item10').css('display', 'inline-block');
+      $('#item11').css('display', 'none');
+      $('#item12').css('display', 'none');
     })
     $('.part6').on('click', function(){
       // console.log('works');
-      $('.third-section1').css('display', 'none');
-      $('.third-section2').css('display', 'none');
-      $('.third-section3').css('display', 'inline-block');
+      $('#item7').css('display', 'none');
+      $('#item8').css('display', 'none');
+      $('#item9').css('display', 'none');
+      $('#item10').css('display', 'none');
+      $('#item11').css('display', 'inline-block');
+      $('#item12').css('display', 'inline-block');
     })
 
   // Generate Movie Posters & Titles to 'Top 10 Moives of All Time'
@@ -170,6 +205,30 @@ $(document).ready(function(){
       }
     }
   });
+  
+
+  // On click for clicking posters
+  $('.movie-poster').on('click', function(){
+    console.log('working');
+    // Hides Front Page and displays movie info screen
+    $('.movie-info-screen').css('display', 'inline-block');
+    $('.carousel').css('display', 'none');
+    $('.first-page').css('display', 'none');
+
+    var movies = response.results
+    var clicked = $(this).attr('data-id');
+    // Display Poster
+    $('.poster').html("<img src='http://image.tmdb.org/t/p/w185/" + movies[clicked].poster_path + "'></img>");
+    // Display Title
+    $('.title').html(movies[clicked].title);
+    // Display Overview
+    $('.overview').html('<br>' + movies[clicked].overview);
+    // Display Overview
+    $('.release-date').html('<br>' + 'Release Date: ' + movies[clicked].release_date);
+    // Display Overview
+    $('.vote-average').html('<br>' + 'Voters Average: ' + movies[clicked].vote_average);
+  })
+
     // Pagination for Top 10
     $('.part7').on('click', function(){
       $('.fourth-section1').css('display', 'inline-block');
