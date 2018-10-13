eb8e29bb724c03236fed28924278b48725cf6d98
Mark <contact.mshin@gmail.com>
2018-10-13 11:45:49 -0600

    fun facts

--- assets/javascript/movie.js
+++ assets/javascript/movie.js
@@ -41,7 +41,6 @@ $(document).ready(function(){
       }
     }
     $('.movie-poster-section2').on('click', function(){
-      console.log('working');
       // Hides Front Page and displays movie info screen
       $('#movie-info-screen').show();
       $('.carousel').css('display', 'none');
@@ -61,6 +60,26 @@ $(document).ready(function(){
       $('.vote-average').html('<br>' + 'Voters Average: ' + movies[clicked].vote_average);
       // Getting Back to top of the page
       $('html,body').scrollTop(0);
+      // Hiding Coming Soon / Now Playing / Top 10
+      $('#comingSoon').css('display', 'none');
+      $('#nowPlaying').css('display', 'none');
+      $('#top10').css('display', 'none');
+      // Displaying Movie Fun Facts
+      var funFacts = [
+        "Guardians Of The Galaxy - Chris Pratt apparently stole his Star-Lord costume from the set, for the sole purpose of having it available so he could show up in costume to visit sick children in the hospital, who might want to meet Star-Lord.", 
+        "The Theory Of Everything - In an e-mail to director James Marsh about the portrayal by Eddie Redmayne, Stephen Hawking said there were certain points when he thought he was watching himself. In addition to his copyrighted voice, Stephen Hawking also lent the filmmakers his Companion of Honour medal and his signed thesis to use as genuine props in the film.", 
+        "Armageddon - NASA shows this film during their management training program. New managers are given the task of trying to spot as many errors as possible. At least one hundred sixty-eight have been found.", 
+        "Dallas Buyers Club - The film's budget was so low that the makeup budget was $250. The film's artists were able to work with that, and the film's Makeup and Hairstyling won an Oscar.", 
+        "The Godfather - The cat held by Marlon Brando in the opening scene was a stray that Coppola found while on the lot at Paramount, and was not originally called for in the script. So content was the cat that its purring muffled some of Brando's dialogue, and, as a result, most of his lines had to be looped.", 
+        "The Matrix - Kung Fu choreographer Woo-Ping Yuen initially refused to work on the film, and hoped that by asking for an exorbitant fee, it would turn off the Wachowskis. It didn't. He next formulated what he considered an impossible request. He said that he'd agree only if he had complete control of the fights, and that he trained the actors for four months before they shoot. The Wachowskis complied with his request.", 
+        "Elizabeth: The Golden Age - In the film, when Elizabeth arrives at St. Paul's Cathedral, construction is going on. In real life, St. Paul's actually needed repair work. Director Shekhar Kapur decided to improvise, and gave the workers costumes and period tools to cut real stone that was being installed in the cathedral. The workers in the scene are real-life stonemasons and construction workers.", 
+      ]
+      var random = funFacts[Math.floor(Math.random()*funFacts.length)];
+      for (var i = 0; i < funFacts.length; i++){
+        console.log('hi');
+        $('.intro').html('Fun Fact!')
+        $('.fun-facts').text(random);
+      }
     })
 
     var movieTitle = response.results[x].title
@@ -100,6 +119,11 @@ $(document).ready(function(){
     $('#item3').css('display', 'flex');
   })
 
+  // var paginationArr = [];
+  // $('#previous').on('click', function(){
+  //   $().
+  // });
+
   // Generate Movie Posters & Title to 'Coming Soon'
   apiKey="1d04e76fc51c2c152a4e8afb75a23a2a";
   queryURL="https://api.themoviedb.org/3/discover/movie?primary_release_date.lte=2018-12-31&primary_release_date.gte=2018-10-16&page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=" + apiKey;
@@ -109,20 +133,20 @@ $(document).ready(function(){
     method: "GET"
   }).then(function(response){  
     console.log(response);
-    for (var x = 0; x < 6; x++){
+    for (var x = 0; x < 5; x++){
       if(response.results[x].poster_path !== null){
         $('#item4').append("<img id='movie2" + x + "' src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
         $('#movie2'+ x).addClass('movie-poster-section3');
         $('#movie2' + x).attr('data-id', x)      }
     }
-    for (var x = 6; x < 11; x++){
+    for (var x = 5; x < 9; x++){
       if(response.results[x].poster_path !== null){
         $('#item5').append("<img id='movie2" + x + "' src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
         $('#movie2'+ x).addClass('movie-poster-section3');
         $('#movie2' + x).attr('data-id', x)
       }
     }
-    for (var x = 11; x < 15; x++){
+    for (var x = 9; x < 14; x++){
       if(response.results[x].poster_path !== null){
         $('#item6').append("<img id='movie2" + x + "' src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
         $('#movie2'+ x).addClass('movie-poster-section3');
@@ -190,14 +214,14 @@ $(document).ready(function(){
         $('#movie3' + x).attr('data-id', x)
       }
     }
-    for (var x = 4; x < 8; x++){
+    for (var x = 4; x < 7; x++){
       if(response.results[x].poster_path !== null){
         $('#item8').append("<img id='movie3" + x + "' src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
         $('#movie3'+ x).addClass('movie-poster-section4');
         $('#movie3' + x).attr('data-id', x)
       }
     }    
-    for (var x = 8; x < 12; x++){
+    for (var x = 7; x < 10; x++){
       if(response.results[x].poster_path !== null){
         $('#item9').append("<img id='movie3" + x + "' src='http://image.tmdb.org/t/p/w185/" + response.results[x].poster_path + "'></img>");
         $('#movie3'+ x).addClass('movie-poster-section4');
@@ -252,13 +276,13 @@ $(document).ready(function(){
     $('#homescreen').on('click', function(){
       window.location.reload(true);
     });
-    $('#nowPlaying').on('click', function(){
-      window.location.reload(true);
-    });
-    $("#comingSoon").on('click', function(){
-      window.location.reload(true);
-    });
-    $("#top10").on('click', function(){
-      window.location.reload(true);
-    });
+    // $('#nowPlaying').on('click', function(){
+    //   window.location.reload(true);
+    // });
+    // $("#comingSoon").on('click', function(){
+    //   window.location.reload(true);
+    // });
+    // $("#top10").on('click', function(){
+    //   window.location.reload(true);
+    // });
 });
